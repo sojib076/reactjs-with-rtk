dcfe8dfa08e637a55a2b00d543a768af797f8fba
sojib076 <sojibds332@gmail.com>
2024-07-08 17:57:21 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import { toast } from "sonner";
+import { Button } from "./components/ui/button";
+import {
+  decrement,
+  increment,
+  incrementByValue,
+} from "./redux/features/counter/counterSlice";
+import { useAppDispatch, useAppSelector } from "./redux/hooks";
+
+const App = () => {
+  const dispatch = useAppDispatch();
+  const count = useAppSelector((state) => state.counter.value);
+  const handelsooner =()=>{
+  
+    toast(<div>
+      <h1>hello</h1>
+      <p>world</p>
+    </div>)
+  }
+  return (
+    <div className="flex h-screen w-full justify-center items-center ">
+      <div className="flex items-center border border-sky-500 rounded-md p-12 gap-2">
+        <Button className="bg-green-500" onClick={() => dispatch(increment())}>
+   
+          Increment
+        </Button>
+        <Button className="bg-green-500" onClick={handelsooner}/>
+        <Button
+          className="bg-blue-500"
+          onClick={() => dispatch(incrementByValue(5))}
+        >
+          Increment By 5
+        </Button>
+        <h1 className="mx-10">{count}</h1>
+        <Button className="bg-red-500" onClick={() => dispatch(decrement())}>
+          Decrement
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import { persistor, store } from "./redux/store.ts";
+import { PersistGate } from "redux-persist/integration/react";
+import { Toaster } from "sonner";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+      <Toaster />
+    </PersistGate>
+    </Provider>
+  </React.StrictMode>
+);
